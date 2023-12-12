8d021c0531321374b2e71cfdb2ae3551911b59d4
Yago Silva <yago.mdk284@gmail.com>
2023-12-11 23:02:33 -0300

    Include Category validations

--- ArchMvc.Domain/Entities/Category.cs
+++ ArchMvc.Domain/Entities/Category.cs
@@ -1,3 +1,6 @@
+
+using ArchMvc.Domain.Validation;
+
 namespace ArchMvc.Domain.Entities;
 
 public sealed class Category
@@ -7,14 +10,27 @@ public sealed class Category
 
     public Category(string name)
     {
-        Name = name;
+        ValidateDomain(name);
     }
 
     public Category(int id, string name)
     {
+        DomainExceptionValidation.When(id < 0, "Invalid Id value.");
+
         Id = id;
         Name = name;
     }
 
     public ICollection<Product> Products { get; private set; }
+
+    private void ValidateDomain(string name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+            "Invalid name. Name is required!");
+
+        DomainExceptionValidation.When(name.Length < 3, 
+            "Invalid name. Too short. Minimun 3 characters!");
+
+        Name = name;
+    }
 }
