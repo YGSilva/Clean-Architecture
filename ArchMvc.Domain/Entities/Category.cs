f4a36d6b4ab11cefc18392558f0b77bf8a3ae1f2
Yago Silva <yago.mdk284@gmail.com>
2023-12-14 20:39:46 -0300

    Added more validation and base class

--- ArchMvc.Domain/Entities/Category.cs
+++ ArchMvc.Domain/Entities/Category.cs
@@ -3,22 +3,23 @@ using ArchMvc.Domain.Validation;
 
 namespace ArchMvc.Domain.Entities;
 
-public sealed class Category
+public sealed class Category : Entity
 { 
-    public int Id { get; private set; }
     public string Name { get; private set; }
 
-    public Category(string name)
-    {
-        ValidateDomain(name);
-    }
-
+    public Category(string name) => ValidateDomain(name);
+     
     public Category(int id, string name)
     {
         DomainExceptionValidation.When(id < 0, "Invalid Id value.");
 
         Id = id;
         Name = name;
+    }
+
+    public void Update(string name)
+    {
+        ValidateDomain(name);
     }
 
     public ICollection<Product> Products { get; private set; }
