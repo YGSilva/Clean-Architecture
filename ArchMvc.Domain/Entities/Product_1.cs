1d5c4efb56d8eed702499c35f63965c126fa0f8b
ITGS_AD\yago.silva <yago.silva@onlineapp.com.br>
2024-01-29 23:44:30 -0300

    Revert "Exclude the Product_1 class, is not important"
    This reverts commit 7307126315493c89396251e0f8e0dec39c257d0f.

---
+++ ArchMvc.Domain/Entities/Product_1.cs
@@ -0,0 +1,35 @@
+using ArchMvc.Domain.Validation;
+
+namespace ArchMvc.Domain.Entities;
+
+public sealed class Product : Entity
+{ 
+    public string Name { get; private set; }
+    public ICollection<Product> Products { get; private set; }
+
+    public Product(string name) => ValidateDomain(name);
+     
+    public Product(int id, string name)
+    {
+        DomainExceptionValidation.When(id < 0, "Invalid Id value.");
+
+        Id = id;
+        Name = name;
+    }
+
+    public void Update(string name)
+    {
+        ValidateDomain(name);
+    }
+
+    private void ValidateDomain(string name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+            "Invalid name. Name is required!");
+
+        DomainExceptionValidation.When(name.Length < 3, 
+            "Invalid name. Too short. Minimun 3 characters!");
+
+        Name = name;
+    }
+}
